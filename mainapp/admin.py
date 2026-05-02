9588f483f8e83a89fa1af64d1f38921f94e69819
Ggtz25 <gabriel.gutierrez06@utrgv.edu>
2026-05-02 13:24:38 -0500

    Final revision for Django movie project

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Advertisement)
+admin.site.register(Celebrity)
+admin.site.register(MovieTheater)
+admin.site.register(MovieTV)
+admin.site.register(News)
+admin.site.register(NewsletterSubscriber)
+admin.site.register(Slider)
+admin.site.register(SocialLink)
+admin.site.register(Trailer)
+admin.site.register(TrailerItem)
+admin.site.register(Tweet)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import SocialLink, Slider, MovieTheater, Advertisement, Celebrity, Trailer, TrailerItem, Tweet, News, NewsletterSubscriber
 
-# Create your views here.
+def home(request):
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+
+        if email:
+            NewsletterSubscriber.objects.get_or_create(email=email)
+
+    social_links = SocialLink.objects.all()
+    sliders = Slider.objects.all()
+    movies_theater = MovieTheater.objects.all()
+    ads = Advertisement.objects.all()
+    celebrities = Celebrity.objects.all()
+    trailers = Trailer.objects.all()
+    trailer_items = TrailerItem.objects.all()
+    tweets = Tweet.objects.all()
+    news_main = News.objects.filter(section="main").first()
+    news_side = News.objects.filter(section="side")
+
+    return render(request, 'index.html', {
+        'social_links': social_links,
+        'sliders': sliders,
+        'movies_theater': movies_theater,
+        'ads': ads,
+        'celebrities': celebrities,
+        'trailers': trailers,
+        'trailer_items': trailer_items,
+        'tweets': tweets,
+        'news_main': news_main,
+        'news_side': news_side,
+    })
